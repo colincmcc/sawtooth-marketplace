0121f2f1f774c5aaa177a57a564b35b8331de6e7
Colin <colincmcc@gmail.com>
2018-09-23 19:49:05 -0400

    validator argument

--- processor/hamletProcessorJs/src/index.js
+++ processor/hamletProcessorJs/src/index.js
@@ -1,7 +1,7 @@
 const { TransactionProcessor } = require('sawtooth-sdk/processor')
 const HamletHandler = require('./handler')
+const address = process.argv[2] || 'tcp://127.0.0.1:4004'
 
-const address = 'tcp://127.0.0.1:4004'
 const transactionProcessor = new TransactionProcessor(address)
 
 transactionProcessor.addHandler(new HamletHandler())
